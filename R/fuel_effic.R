887c3a3bbfbfc02a7c58d7c1947ca0e942305b22
opossumpals <deloresjchan@ucsb.edu>
2024-03-09 15:24:15 -0800

    i think i fixed it?

--- R/fuel_effic.R
+++ R/fuel_effic.R
@@ -12,7 +12,7 @@
 #' @param pair (kg/m3) default =1.2
 #' @return power (W)
 
-autopower = function(V, m, A, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
+fuel_effic = function(V, m, A, cdrag=0.3, crolling=0.015,pair=1.2,g=9.8) {
   P = crolling*m*g*V + 1/2*A*pair*cdrag*V**3
   return(P)
 }
